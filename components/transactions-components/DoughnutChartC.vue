8a9dba7162276547a4e9ddff625fd0c5b4946ccb
fernandooliveiralima <oliveirafernando.contato@gmail.com>
2024-09-22 12:42:45 -0300

    AuthTest-NuxtJs (Insercao dos graficos Finalizado)

--- components/transactions-components/DoughnutChartC.vue
+++ components/transactions-components/DoughnutChartC.vue
@@ -1,11 +1,61 @@
-<script setup>
-    
-</script>
+<script setup lang="ts">
+/* External Libraries */
+import { Chart } from 'chart.js/auto';
+/* External Libraries */
 
+/* Variables Chartjs */
+const myChart = ref < HTMLCanvasElement | null > (null);
+let doughnutChart: Chart<"doughnut", number[], string> | null = null;
+/* Variables Chartjs */
+
+/* Functions ------------------------------------------------------------------------------------------------------------------------------------------------------- */
+
+/* Function Create Doughnut Chart */
+const createChart = () => {
+    if (myChart.value && !doughnutChart) {
+        const ctx = myChart.value.getContext('2d');
+        if (!ctx) return;
+
+        doughnutChart = new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                labels: ['Income', 'Expense'],
+                datasets: [{
+                    data: [10, 5],  // Inicializando com valores padrão
+                    borderWidth: 0,
+                    hoverOffset: 5,
+                    backgroundColor: ['green', 'crimson'],
+                }]
+            },
+            options: {
+                responsive: true,
+                cutout: 130
+            }
+        });
+    }
+};
+/* Function Create Doughnut Chart */
+
+/* Functions ------------------------------------------------------------------------------------------------------------------------------------------------------- */
+
+/* Vue Functions */
+onMounted(() => {
+    createChart();
+})
+/* Vue Functions */
+
+</script>
 
 <template>
     <div class="percentual-value">
-       Doughnut Chart
+
+        <section class="doughnut-chart">
+            <div class="chart">
+                <div class="percentual">Percentual Value</div>
+                <canvas ref="myChart"></canvas>
+            </div>
+        </section>
+
     </div>
 </template>
 
@@ -15,12 +65,32 @@
 @tailwind components;
 @tailwind utilities;
 
-.percentual-value{
-    @apply
-    border border-amber-800
-    h-[20rem]
-    w-[40rem];
+
+@layer components{
+
+    .doughnut-chart{
+        @apply
+        
+        h-[23rem]
+        w-[40rem]
+        flex items-center justify-center;
+    }
+
+    .percentual{
+        @apply
+        absolute
+        top-[45rem]
+        text-xl
+        font-semibold
+        text-white;
+    }
+
+    .chart{
+        @apply
+    
+        h-full
+        w-full
+        flex items-center justify-center;
+    }
 }
-
-
 </style>
--- components/transactions-components/LineChartC.vue
+++ components/transactions-components/LineChartC.vue
@@ -1,12 +1,67 @@
-<script setup>
-    
+<script setup lang="ts">
+/* External Libraries */
+import { Chart } from 'chart.js/auto';
+/* External Libraries */
+
+/* Variables ChartJs */
+const myChart = ref < HTMLCanvasElement | null > (null);
+let lineChart: Chart<"line", number[], string> | null = null;
+/* Variables ChartJs */
+
+/* Functions */
+
+/* Function Create Line Chart */
+const createLineChart = () => {
+  if (myChart.value) {
+    const ctx = myChart.value;
+    lineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['2024-02-23', '2023-04-17'], // Inicialmente vazio
+        datasets: [{
+          label: 'Behavior Transactions',
+          data: [5, 10, 15],
+          fill: false,
+          backgroundColor: [],
+          tension: 0.1,
+          borderColor: ['green']
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            ticks: { display: false },
+            grid: { color: '#000' }
+          },
+          x: { grid: { color: '#000' } }
+        }
+      }
+    });
+  }
+}
+/* Function Create Line Chart */
+
+/* Functions */
+
+/* Vue Functions */
+onMounted(()=>{
+  createLineChart();
+})
+/* Vue Functions */
 </script>
 
 
 <template>
-    <div class="transactions-info-total">
-      transactions Info Total Line Chart
-    </div>
+  <div class="transactions-info-total">
+
+    <section class="line-chart">
+      <div class="chart">
+        <canvas ref="myChart"></canvas>
+      </div>
+    </section>
+
+  </div>
 </template>
 
 
@@ -15,12 +70,28 @@
 @tailwind components;
 @tailwind utilities;
 
+@layer components{
 
-.transactions-info-total{
-    @apply h-[30rem]
-    w-[50rem] 
-    border border-pink-500
-    px-2;
+  .transactions-info-total {
+    @apply h-[30rem] w-[50rem] border border-pink-500 px-2;
+  }
+
+  .line-chart{
+    @apply
+    
+    h-full
+    w-full
+    flex items-center justify-center;
+  }
+
+  .chart{
+    @apply
+    
+    h-full
+    w-full
+    flex items-center justify-center;
+  }
+
 }
 
 
